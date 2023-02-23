29aaa24ba69ad802ca29922ab31b76b22f5612d7
ncuti60@gmail.com <ncuti60@gmail.com>
2023-02-23 23:43:53 +0200

    fix scrolling issue

--- src/components/product/mobile/ProductGallery.vue
+++ src/components/product/mobile/ProductGallery.vue
@@ -74,7 +74,7 @@ export default {};
 
   &__images {
     display: grid;
-    grid-template-columns: repeat(2, 50%);
+    grid-template-columns: repeat(2, auto);
     grid-template-rows: repeat(5, auto);
     gap: 3px;
 
